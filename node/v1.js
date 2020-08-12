3a05550c8a995799012d210e366cb83f1da819c4
Adam McGurk <mcgurk@mcgurkconsulting.com>
2020-08-12 13:13:14 -0500

    Let's see if this works

--- node/v1.js
+++ node/v1.js
@@ -1,6 +1,139 @@
 exports.handler = async (ev, context) => {
-	return {
+
+	const objectReturn = {
 		statusCode: 200,
-		body: JSON.stringify(ev)
+		body: ""
 	};
+
+	if (ev.httpMethod !== "PUT") {
+		objectReturn.statusCode = 405;
+		objectReturn.body = JSON.stringify({
+			"success": false,
+			"message": "Only PUT requests are accepted"
+		});
+		return objectReturn;
+	}
+
+	if (ev.headers["content-type"] !== "application/json" || ev.headers["accept"] !== "application/json") {
+		objectReturn.statusCode = 400;
+		objectReturn.body = JSON.stringify({
+			"success": false,
+			"message": "All requests must include the headers of Accept and Content-Type with the value of 'application/json' for both"
+		});
+		return objectReturn;
+	}
+
+	let jsonBody;
+
+	try {
+		jsonBody = JSON.parse(ev.body);
+	} catch (e) {
+		objectReturn.statusCode = 400;
+		objectReturn.body = JSON.stringify({
+			"success": false,
+			"message": "Something went wrong while trying to parse the JSON body"
+		});
+		return objectReturn;
+	}
+
+	if (jsonBody.font == undefined) {
+		objectReturn.statusCode = 400;
+		objectReturn.body = JSON.stringify({
+			"success": false,
+			"message": "The body is missing the 'font' parameter"
+		});
+		return objectReturn;
+	}
+
+	let Parser;
+
+	try {
+		Parser = new GoogleFontParser(jsonBody.font);
+		const newCode = Parser.parse();
+	} catch (e) {
+		objectReturn.statusCode = 400;
+		objectReturn.body = JSON.stringify({
+			"success": false,
+			"message": e
+		});
+		return objectReturn;
+	}
+
+	objectReturn.body = JSON.stringify({
+		"success": true,
+		"message": "Successfully parsed font!",
+		"font": newCode
+	});
+
+	return objectReturn;
 }
+
+class GoogleFontParser {
+
+	constructor(rawFontString) {
+		this.fontString = rawFontString;
+	}
+
+	parse() {
+
+		let cleanUrl;
+
+		if (this.fontString.startsWith("<link")) {
+			cleanUrl = this.parseLinkTag();
+		} else {
+			cleanUrl = this.parseUrl(this.fontString);
+		}
+
+		return this.addParams(cleanUrl);
+
+	}
+
+	parseUrl(rawUrl) {
+
+		let fullUrl;
+
+		try {
+			fullUrl = new URL(rawUrl);
+		} catch (e) {
+			throw "Sorry, please provide a valid URL";
+		}
+
+		if (fullUrl.host != "fonts.googleapis.com") {
+			throw "Sorry, only Google Fonts are available to optimize right now";
+		}
+
+		return fullUrl.href;
+
+	}
+
+	parseLinkTag() {
+		
+		let rawUrl;
+		const indexOfHref = this.fontString.indexOf('href="') + 6;
+
+		if (indexOfHref == -1) {
+			throw "Sorry, that isn't a valid Google Fonts link tag";
+		}
+
+		const lastIndex = this.fontString.indexOf('"', indexOfHref);
+
+		return this.fontString.substr(indexOfHref, (lastIndex - indexOfHref));
+
+	}
+
+	addParams(cleanUrl) {
+		let addedUrl;
+		const fullUrl = new URL(cleanUrl);
+		const searchParams = new URLSearchParams(fullUrl.search);
+
+		if (searchParams.get("display") == null || searchParams.get("display") != "swap") {
+			addedUrl = fullUrl.href += "&display=swap";
+		} else {
+			addedUrl = fullUrl.href;
+		}
+
+		return addedUrl;
+	}
+
+}
+
