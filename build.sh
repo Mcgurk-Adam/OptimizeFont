6fe16fc26ec376550be242ebea233ace415c1b00
Adam McGurk <eldermcgurk@gmail.com>
2024-02-29 17:59:25 -0700

    the build is done

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir dist/
 mkdir dist/style
 mkdir dist/js
 node_modules/.bin/tsc -p scripts/
-node_modules/.bin/sass --style=compressed style/style.scss > dist/style/style.css
+node_modules/.bin/sass --style=compressed style/scss/style.scss > dist/style/style.css
 
 cp -R assets dist/assets/
 cp js/build.js dist/js/build.js
